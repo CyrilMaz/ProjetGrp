be661516dfedeae44c868a2ba9be05b0b8ddadb9
cyril <cyril.mazauric@ynov.com>
2025-09-15 16:40:46 +0200

    fix DisplayInfo

--- src/personnage.go
+++ src/personnage.go
@@ -14,30 +14,51 @@ type Character struct {
 }
 
 type Items struct {
-	Name string
-	Quantity int
-	StatName string
-	StatBoost int
+	Name            string
+	Quantity        int
+	StatName        string
+	StatBoost       int
 	RespectiveClass string
 }
 
+// Variable globale accessible partout
+var FirstCharacter Character
+
 func initCharacter() {
 	var nom string
-	fmt.Println("Quel est le nom de ton personnage")
-	fmt.Scanln(&nom) // scan le nom du personnage
-	match := true
-	for i := 0; i < len(nom); i++ {
-		if (nom[i] < 97 || nom[i] > 122) && (nom[i] < 65 || nom[i] > 90) {
-			match = false
+	for {
+		fmt.Println("Quel est le nom de ton personnage")
+		fmt.Scanln(&nom) // scan le nom du personnage
+		match := true
+		for i := 0; i < len(nom); i++ {
+			if (nom[i] < 97 || nom[i] > 122) && (nom[i] < 65 || nom[i] > 90) {
+				match = false
+			}
+		}
+		if match == false {
+			fmt.Println("Ton nom ne peut contenir que des lettres ! : A_Z, a_z")
+			fmt.Println("réessaie !")
+		} else {
+			break
 		}
 	}
-	if match == false { 
-		fmt.Println("Ton nom ne peut contenir que des lettres ! : A_Z, a_z")
-		fmt.Println("réessaie !")
-		fmt.Scanln(&nom)
+	PotionSoin := Items{"potions de soin", 3, "soin", 20, "all"}
+	FirstCharacter = Character{nom, "Elfe", 1, 40, 100, []Items{PotionSoin}} // = au lieu de :=
+	fmt.Println("tu as désormais", FirstCharacter.Inventaire[0].Quantity, FirstCharacter.Inventaire[0].Name)
+}
+
+func DisplayInfo(p Character) {
+	fmt.Println("Nom :", p.Name)
+	fmt.Println("Classe :", p.Class)
+	fmt.Println("Niveau :", p.Level)
+	fmt.Println("Points de vie actuels :", p.Pv)
+	fmt.Println("Points de vie max :", p.Pvmax)
+	for i := 0; i < len(p.Inventaire); i++ {
+		fmt.Println("tu as", p.Inventaire[i].Quantity, p.Inventaire[i].Name)
 	}
-	fmt.Println("Ton nom est : " + nom)
-	PotionSoin := Items{"potion de soin", 3, "soin", 20, "all"}
-	FirstCharacter := Character{nom, "Elfe", 1, 40, 100, []Items{PotionSoin}}
-	fmt.Println(FirstCharacter.Inventaire[0].Name, FirstCharacter.Inventaire[0].Quantity)
+}
+
+func main() {
+	initCharacter()
+	DisplayInfo(FirstCharacter) // Maintenant FirstCharacter est accessible
 }
