788461cee41e4bb29bb2e61477cff09736635a5a
cyril <cyril.mazauric@ynov.com>
2025-09-15 12:45:02 +0200

    modify personnage.go

--- src/personnage.go
+++ src/personnage.go
@@ -1,4 +1,4 @@
-package ProjetGrp
+package main
 
 import "fmt"
 
@@ -11,8 +11,20 @@ type Character struct {
 	inventaire []string
 }
 
-func initCharacter() {
-	nom := Character.Name
+func main() {
+	var nom string
 	fmt.Println("Quel est le nom de ton personnage")
-	fmt.Scanln(&nom)
+	fmt.Scanln(&nom) // scan le nom du personnage
+	match := true
+	for i := 0; i < len(nom); i++ {
+		if (nom[i] < 97 || nom[i] > 122) && (nom[i] < 65 || nom[i] > 90) {
+			match = false
+		}
+	}
+	if match == false { 
+		fmt.Println("Ton nom ne peut contenir que des lettres ! : A_Z, a_z")
+		fmt.Println("réessaie !")
+		fmt.Scanln(&nom)
+	}
+	fmt.Println("Ton nom est : " + nom)
 }
