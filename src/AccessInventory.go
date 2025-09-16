0f699db8e29b4f36a53508f5a84f51ee91a295d1
cyril <cyril.mazauric@ynov.com>
2025-09-16 16:33:13 +0200

    fix

--- src/AccessInventory.go
+++ src/AccessInventory.go
@@ -2,13 +2,13 @@ package main
 
 import "fmt"
 
-var answer1 int 
+var answer1 int
 var answer2 string
 var UseItem bool = false 
 
 /*#############################
-  # AFFICHAGE DE L'INVENTAIRE #
-  #############################*/
+# AFFICHAGE DE L'INVENTAIRE #
+#############################*/
 func accessInventory(FirstCharacter Character) {
 	fmt.Println("===============INVENTAIRE================")
 	for i := 0; i < len(FirstCharacter.Inventory); i++ {
@@ -17,13 +17,14 @@ func accessInventory(FirstCharacter Character) {
 
 
 	/*######################
-	  # GESTION DES OBJETS #
-	  ######################*/
+	# GESTION DES OBJETS #
+	######################*/
 	fmt.Println("================ACTIONS==================")
-	fmt.Println("tapez un numéro pour utiliser un objet, sinon tapez \"exit\"")
+	fmt.Println("tapez un numéro pour utiliser un objet, sinon tapez \"Exit\"")
 	fmt.Scanln(&answer1)
+	
 	switch answer1 {
-	case 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10:	
+	case 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10:
 		if FirstCharacter.Inventory[answer1].Type == "Consumable" {
 			fmt.Println("êtes-vous certain de vouloir consommer un(e)", FirstCharacter.Inventory[answer1].Name, "? [O/N]")
 			fmt.Scanln(&answer2)
@@ -31,22 +32,20 @@ func accessInventory(FirstCharacter Character) {
 			case "O", "o", "Oui", "oui":
 				FirstCharacter.Inventory[answer1].Quantity -= 1
 				fmt.Println("consommé")
-				accessInventory(FirstCharacter)
 				TakePot(FirstCharacter) // lien vers la fonction TakePot
+				accessInventory(FirstCharacter)
 			case "N", "n", "non", "no", "No", "Non":
 				fmt.Println("annulé")
 				accessInventory(FirstCharacter)
 			default:
-				fmt.Println("Erreur, Réponse non appropriée -> Retour au menu précédent")
+				fmt.Println("Retour au menu précédent")
 			}
 		} else if FirstCharacter.Inventory[answer1].Type == "Equippable" {
 			// il faut faire la même chose pour les objets équipalbe comme outil, arme, arumure
 		} else {
 			fmt.Println("Erreur, l'objet sélectionné ne peut pas être utilisé")
 		}
-	case "exit":
-		return
 	default:
-		fmt.Println("Erreur, Réponse non prise en charge")
+		return
 	}
 }
