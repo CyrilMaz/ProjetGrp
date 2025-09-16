b4b362dabadf949e5b915dca800cd9ffc69fbfde
cyril <cyril.mazauric@ynov.com>
2025-09-16 12:06:17 +0200

    AccessInventory.go

--- src/AccessInventory.go
+++ src/AccessInventory.go
@@ -3,28 +3,40 @@ package main
 import "fmt"
 
 func accessInventory(FirstCharacter Character) {
-	fmt.Println("0 : sortir")	
-
+	fmt.Println("===============INVENTAIRE================")
 	for i := 0; i < len(FirstCharacter.Inventory); i++ {
-		fmt.Println(i, ":", FirstCharacter.Inventory[i].Name, FirstCharacter.Inventory[i].Quantity)
+		fmt.Println(i, ":", FirstCharacter.Inventory[i].Name,", quantité :",FirstCharacter.Inventory[i].Quantity)
 	}
-
+	fmt.Println("tapez un numéro pour utiliser un objet, sinon tapez \"exit\"")
 	fmt.Scanln(&answer)
+	if FirstCharacter.Inventory[answer].Consumables == true {
+		FirstCharacter.Inventory[answer].Quantity -= 2
+	}
 
 	switch answer {
 	case "0":
-		return
+
 	case "1":
+
 	case "2":
+		
 	case "3":
+
 	case "4":
+
 	case "5":
+
 	case "6":
+
 	case "7":
+
 	case "8":
+
 	case "9":
+
 	case "10":
-	default:
-		fmt.Println("Choix invalide")
+
+	case "exit":
+		return
 	}
 }
