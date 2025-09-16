a2157b6e78c0d311a3a83b26f9b24890486b3388
cyril <cyril.mazauric@ynov.com>
2025-09-16 22:50:59 +0200

    modify

--- src/DisplayInfo.go
+++ src/DisplayInfo.go
@@ -4,14 +4,14 @@ import "fmt"
 
 func DisplayInfo(p Character) {
 	fmt.Println("\n")
-	fmt.Println("======INFORMATIONS======")
+	fmt.Println("◇─◇──◇────◇ INFORMATION ◇─────◇──◇─◇")
 	fmt.Println("Nom :", p.Name)
 	fmt.Println("Classe :", p.Class)
 	fmt.Println("Niveau :", p.Level)
 	fmt.Println("Points de vie actuels :", p.Pv)
 	fmt.Println("Points de vie max :", p.Pvmax)
 	fmt.Println("\n")
-	fmt.Println("======INVENTAIRE======")
+	fmt.Println("◇─◇──◇────◇ INVENTAIRE ◇─────◇──◇─◇")
 	for i := 0; i < len(p.Inventory); i++ {
 		fmt.Println("tu as", p.Inventory[i].Quantity, p.Inventory[i].Name)
 	}
--- src/TakePot.go
+++ src/TakePot.go
@@ -2,11 +2,11 @@ package main
 
 import "fmt"
 
-func TakePot(c Character) {
+func TakePot(c *Character) {
 	if c.Pv+50 > c.Pvmax {
-		c.Pv = c.Pvmax
+		*c.Pv = c.Pvmax
 	} else {
-		c.Pv += 50
+		*c.Pv += 50
 	}
 	fmt.Println(c.Pv, "HP /", c.Pvmax, "HP")
 }
