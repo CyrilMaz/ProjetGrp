eb6981598405c2c1f7e371d8adea6feb085c48a5
matquentin <quentin.mateus@ynov.com>
2025-09-16 23:46:23 +0200

    add free potion

--- src/Merchand.go
+++ src/Merchand.go
@@ -2,25 +2,36 @@ package main
 
 import "fmt"
 
-var choice int 
+var PotionGratuite Bool = True // Variable globale pour suivre si la potion gratuite a été prise
 
 func Merchand() {
 	fmt.Println("Bienvenue chez le marchand !")
 	fmt.Println("Que souhaitez-vous acheter ?")
+	fmt.Println("1 : Potion de soin (20 Argent)")
 	fmt.Println("0 : Quitter le marchand")
-	fmt.Println("1 : Potion de soin (20 pièces d'or)")
+
+	var choice int 
 	fmt.Scanln(&choice) // Lire le choix de l'utilisateur
 
 	switch choice {
 	case 1:
-		fmt.Println("Vous avez acheté une potion de soin.")
-		FirstCharacter.Inventory = append(FirstCharacter.Inventory, PotionSoin) //Ajoute la potion de soin à l'inventaire via la fonction append
-		// Ici, vous pouvez ajouter la logique pour déduire l'argent du personnage
+		if PotionGratuite {
+			fmt.Println("Vous avez reçu une potion de soin gratuite !")
+			FirstCharacter.Inventory = append(FirstCharacter.Inventory, PotionSoin) //Ajoute la potion de soin à l'inventaire via la fonction append
+			PotionGratuite = false // Met à jour la variable pour indiquer que la potion gratuite a été prise
+		} else {
+			if FirstCharacter.Argent >= 20 {
+				FirstCharacter.Inventory = append(FirstCharacter.Inventory, PotionSoin) //Ajoute la potion de soin à l'inventaire via la fonction append
+				FirstCharacter.Argent -= 20 // Déduit le coût de la potion de l'argent du personnage
+				fmt.Println("Vous avez acheté une potion de soin.")
+			} else {
+				fmt.Println("Vous n'avez pas assez d'argent pour acheter une potion de soin.")
+			}
 	case 0:
 		fmt.Println("Merci de votre visite !")//
 		main() // Retour au menu principal
 	default: // fonction default sert à gérer les cas non prévus
-		fmt.Println("Choix invalide, réessayez.")
-		fmt.Scanln(&choice)
+		fmt.Println("Choix invalide, réessayez.")// 
+		fmt.Scanln(&choice) // Lire le choix de l'utilisateur
 	}
 }
