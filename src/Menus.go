927c2aceedefab2102d6e67d1072b735e4b67a09
cyril <cyril.mazauric@ynov.com>
2025-09-16 16:52:32 +0200

    modify

--- src/Menus.go
+++ src/Menus.go
@@ -1,6 +1,6 @@
 package main
 
-var MENU01 = []string{"0 : sortir", "1 : infos du personnage", "2 : accéder à l'inventaire"}
+var MENU01 = []string{"0 : sortir", "1 : infos du personnage", "2 : accéder à l'inventaire", "3 : Marchand"}
 var MENU02 = FirstCharacter.Inventory
 
 
