20d5593aec033c1d39c150558888b00260f31100
cyril <cyril.mazauric@ynov.com>
2025-09-16 11:22:20 +0200

    modify AccessInventory.go

--- src/AccessInventory.go
+++ src/AccessInventory.go
@@ -3,7 +3,32 @@ package main
 import "fmt"
 
 func accessInventory(FirstCharacter Character) {
-	for i := 0; i < len(FirstCharacter.Inventory); i++ {
-		fmt.Println(FirstCharacter.Inventory[i].Name, FirstCharacter.Inventory[i].Quantity)
+	fmt.Println("0 : sortir")	
+	for i := 1; i < len(FirstCharacter.Inventory) - 1; i++ {
+		fmt.Println(i, ":", FirstCharacter.Inventory[i].Name, FirstCharacter.Inventory[i].Quantity)
+	}
+	fmt.Scanln(&answer)
+	switch answer {
+	case "0":
+		return
+	case "1":
+
+	case "2":
+
+	case "3":
+
+	case "4":
+
+	case "5":
+
+	case "6":
+
+	case "7":
+
+	case "8":
+
+	case "9":
+
+	case "10":
 	}
 }
