c4687bb99b32360b4f466025dc30db146afc1949
cyril <cyril.mazauric@ynov.com>
2025-09-17 09:50:25 +0200

    add RemoveInventory.go

---
+++ src/RemoveInventory.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func RemoveInventory(p *Character, i Items) {
+	for j := 0; j < len(p.Inventory); j++ {
+		if p.Inventory[j].Name == i.Name {
+			if p.Inventory[j].Quantity > 0 {
+				p.Inventory[j].Quantity--
+				if p.Inventory[j].Type == "consommable" {
+					fmt.Println("Vous avez utilisé une", i.Name)
+				} else if p.Inventory[j].Type == "équipement" {
+					fmt.Println("Vous avez équipé", i.Name)
+				} else {
+					fmt.Println("Vous avez enlevé", i.Name, "de votre inventaire")
+				}
+			}
+		} else {
+			fmt.Print("vous ne possédez pas cet item dans votre inventaire")
+		}
+	}
+}
