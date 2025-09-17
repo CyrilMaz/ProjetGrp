0275543261fe233d1e9d1dd1750fd93646d8acfb
cyril <cyril.mazauric@ynov.com>
2025-09-17 11:12:17 +0200

    add PoisonPot

---
+++ src/PoisonPot.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func PoisonPot(p *Character) {
+	fmt.Println("Vous avez bu une potion empoisonnée !")
+	time.Sleep(3 * time.Second)
+	fmt.Println("Vous perdez 10 points de vie.")
+	p.Pv -= 10
+	if p.Pv <= 0 {
+		fmt.Println("Vous êtes mort !")
+		IsDead(p) // Appelle la fonction IsDead pour gérer la mort du personnage
+	} else {
+		fmt.Println("Il vous reste", p.Pv, "/", p.Pvmax, "points de vie.")
+	}
+}
