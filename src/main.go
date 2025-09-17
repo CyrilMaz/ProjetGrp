b516f6ab2e6d07cded58550c9af0735e9aeb867c
cyril <cyril.mazauric@ynov.com>
2025-09-17 14:30:28 +0200

    Modify menu display

--- src/main.go
+++ src/main.go
@@ -6,17 +6,35 @@ var initalized bool = false
 var answer string
 
 func main() {
+	for spaces := 0; spaces < 40; spaces++ {
+		fmt.Print("\n")
+	}
 	if !initalized {
 		initCharacter()
 		initalized = true
+		fmt.Println("╔══════════════════════════════════════════════╗")
+		fmt.Println("║                                              ║")
+		fmt.Println("║      BIENVENUE DANS LE MONDE D'AVENTURE      ║")
+		fmt.Println("║                                              ║")
+		fmt.Println("║        ──◇◆◇──  Projet Groupe  ──◇◆◇──       ║")
+		fmt.Println("║                                              ║")
+		fmt.Println("║    Prépare-toi à forger ton destin, héros !  ║")
+		fmt.Println("║                                              ║")
+		fmt.Println("╚══════════════════════════════════════════════╝")
 	}
 	for answer != "0" {
-		fmt.Println("\nQue voulez-vous faire ?")
+		fmt.Println("★━━━━━━━━━━━━━━━━━━━━━━━━━★")
+		fmt.Println("| Que voulez-vous faire ? |")
+		fmt.Println("★━━━━━━━━━━━━━━━━━━━━━━━━━★")
 		for i := 0; i < len(MENU01); i++ {
 			fmt.Println(MENU01[i])
 		}
-
-		fmt.Print("Votre choix : ")
+		fmt.Print("\n")
+		fmt.Println(MENU00)
+		fmt.Print("\n")
+		fmt.Println("★━━━━━━━━━━━━━━━━━━━━━━★")
+		fmt.Println("| Quel est ton choix ? |")
+		fmt.Println("★━━━━━━━━━━━━━━━━━━━━━━★")
 		fmt.Scanln(&answer)
 
 		switch answer {
