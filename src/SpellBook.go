49dad57a2d0bffb6659c2e8104131526b708fd94
bilel <bilel@Ordinateur-portable-de-bilel.local>
2025-09-17 12:40:57 +0200

    fix spell

--- src/SpellBook.go
+++ src/SpellBook.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func SpeellBook(p *Character, s Skills) {
-	for i := 0; i < len((*p).Skills); i++ {
+	for i := 0; i > len((*p).Skills); i++ {
 		if (*p).Skills[i].Name == s.Name {
 			s.learned = true
 			fmt.Println("Vous avez appris le sort :", s.Name)
