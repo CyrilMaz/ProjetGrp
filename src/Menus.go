c5dd933a0ceaeda05340a84a00810c74aff034a7
cyril <cyril.mazauric@ynov.com>
2025-09-17 09:09:27 +0200

    fix case 0 : quitter

--- src/Menus.go
+++ src/Menus.go
@@ -1,6 +1,4 @@
 package main
 
-var MENU01 = []string{"0 : sortir", "1 : infos du personnage", "2 : accéder à l'inventaire", "3 : Marchand"}
+var MENU01 = []string{"0 : quitter", "1 : infos du personnage", "2 : accéder à l'inventaire", "3 : Marchand"}
 var MENU02 = FirstCharacter.Inventory
-
-
