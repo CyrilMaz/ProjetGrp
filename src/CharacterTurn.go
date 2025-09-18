c021b275b1cda10325c99e5107f8c77424a3ba7c
cyril <cyril.mazauric@ynov.com>
2025-09-18 16:58:41 +0200

    fix CharacterTurn display & error display

---
+++ src/CharacterTurn.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+var choice int
+
+func CharacterTurn(p *Character) {
+
+	fmt.Println("★¸„.-•~¹°”ˆ˜¨ ACTIONS ¨˜ˆ”°¹~•-.„¸★")
+	fmt.Println("|         C'est ton tour !        |")
+	fmt.Println("★━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━★")
+	fmt.Println("|1:··········Attaquer             |")
+	fmt.Println("|2:······Voir l'inventaire        |")
+	fmt.Println("|                                 |")
+	fmt.Println("★━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━★")
+	fmt.Println("|       0 : Fuir du combat        |")
+	fmt.Println("★━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━★")
+	if lasterror != "" {
+		fmt.Println("Erreur :", lasterror)
+		lasterror = ""
+	}
+
+	fmt.Scanln(&choice) // Lire le choix de l'utilisateur
+
+	switch choice {
+	case 1:
+		fmt.Println(p.Name, "attaques")
+		// faire la fonction d'attaque ici
+	case 2:
+		fmt.Println(p.Name, "inventaire !")
+		accessInventory(p) // Accède à l'inventaire pour le consulter
+	case 0:
+		fmt.Println(p.Name, "battre en retraite")
+		// Logique de passage de tour ici
+	default:
+		lasterror = "Choix invalide, veuillez réessayer."
+		CharacterTurn(p) // Re-demande le choix si invalide
+	}
+}
