3cde926c70a636c0aceb23955ddd0cb173546bd5
cyril <cyril.mazauric@ynov.com>
2025-09-19 00:02:41 +0200

    fix case goes 1 -> 40 instead of 1 -> 10

--- src/AccessInventory.go
+++ src/AccessInventory.go
@@ -50,7 +50,7 @@ func accessInventory(p *Character) {
 	fmt.Scanln(&answer1)
 
 	switch answer1 {
-	case 1, 2, 3, 4, 5, 6, 7, 8, 9, 10:
+	case 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40:
 		if p.Inventory[answer1-1].Type == "Consumable" {
 			fmt.Println("★ êtes-vous certain de vouloir consommer un(e)", p.Inventory[answer1-1].Name, "? [O/N]")
 			fmt.Scanln(&answer2)
