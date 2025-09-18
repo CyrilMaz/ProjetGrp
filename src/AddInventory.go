202e36d22f79ec515540a96fbc59cc7b7653eea1
cyril <cyril.mazauric@ynov.com>
2025-09-18 10:21:21 +0200

    fix display nbr inv

--- src/AddInventory.go
+++ src/AddInventory.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+var found bool = false
+
 func AddInventory(p *Character, i Items) { // Ajoute un item à l'inventaire du personnage , si jamais l'item est déjà dans l'inventaire, incrémente la quantité
 	if len(p.Inventory) == 0 { // si l'inventaire est vide, on ajoute l'item directement
 		p.Inventory = append(p.Inventory, i) // ajoute l'item à l'inventaire via la fonction append
@@ -11,18 +13,22 @@ func AddInventory(p *Character, i Items) { // Ajoute un item à l'inventaire du 
 	} else if !InventoryLimitCheck(p) { // si l'inventaire est plein, on ne peut pas ajouter d'item
 		fmt.Println("Votre inventaire est plein, vous ne pouvez pas ajouter d'item.")
 	} else {
+		found = false
 		for j := 0; j < len(p.Inventory); j++ { // cette boucle fort incrémente la quantité d'un item si il est déjà dans l'inventaire
-			if p.Inventory[j].Name == i.Name { // compare le nom de l'item à ajouter avec les items déjà dans l'inventaire
-				p.Inventory[j].Quantity += 1 // incrémente la quantité de l'item
+			if p.Inventory[j].Name == i.Name { // si l'item est déjà dans l'inventaire, on incrémente la quantité
+				p.Inventory[j].Quantity += i.Quantity // incrémente la quantité de l'item
+				found = true
 				if DisplayAddItem {
 					fmt.Println("Vous avez ajouté", i.Quantity, i.Name, "à votre inventaire.")
 				}
-			} else if j == len(p.Inventory) { // si l'item n'est pas dans l'inventaire, on l'ajoute
-				p.Inventory = append(p.Inventory, i) // ajoute l'item à l'inventaire via la fonction append
-				if DisplayAddItem {
-					fmt.Println("Vous avez ajouté", i.Quantity, i.Name, "à votre inventaire.")
-				}
+				break
 			}
+		}
+	}
+	if !found {
+		p.Inventory = append(p.Inventory, i) // ajoute l'item à l'inventaire via la fonction append
+		if DisplayAddItem {
+			fmt.Println("Vous avez ajouté", i.Quantity, i.Name, "à votre inventaire.")
 		}
 	}
 	DisplayAddItem = true
--- src/Items.go
+++ src/Items.go
@@ -1,12 +1,12 @@
 package main
 
 var PotionSoin = Items{"potions de soin", 1, "soin", 20, "all", "Consumable"}
-var LivreSorts = Items{"Livre de sorts", 0, "FireBall", 0, "all", "Consumable"}
-var PotionPoison = Items{"Potion de poison", 0, "poison", 0, "all", "Consumable"}
-var FourrureLoup = Items{"Fourrure de loup", 0, "craft", 0, "all", "Material"}
-var PeauTroll = Items{"Peau de troll", 0, "craft", 0, "all", "Material"}
-var CuirSanglier = Items{"Cuir de sanglier", 0, "craft", 0, "all", "Material"}
-var PlumeCorbeau = Items{"Plume de corbeau", 0, "craft", 0, "all", "Material"}
-var ChapeauAventurier = Items{"Chapeau de l’aventurier", 0, "armor", 0, "all", "Equipment"}
-var TuniqueAventurier = Items{"Tunique de l’aventurier", 0, "armor", 0, "all", "Equipment"}
-var BottesAventurier = Items{"Bottes de l’aventurier", 0, "armor", 0, "all", "Equipment"}
+var LivreSorts = Items{"Livre de sorts", 1, "FireBall", 0, "all", "Consumable"}
+var PotionPoison = Items{"Potion de poison", 1, "poison", 0, "all", "Consumable"}
+var FourrureLoup = Items{"Fourrure de loup", 1, "craft", 0, "all", "Material"}
+var PeauTroll = Items{"Peau de troll", 1, "craft", 0, "all", "Material"}
+var CuirSanglier = Items{"Cuir de sanglier", 1, "craft", 0, "all", "Material"}
+var PlumeCorbeau = Items{"Plume de corbeau", 1, "craft", 0, "all", "Material"}
+var ChapeauAventurier = Items{"Chapeau de l’aventurier", 1, "armor", 0, "all", "Equipment"}
+var TuniqueAventurier = Items{"Tunique de l’aventurier", 1, "armor", 0, "all", "Equipment"}
+var BottesAventurier = Items{"Bottes de l’aventurier", 1, "armor", 0, "all", "Equipment"}
